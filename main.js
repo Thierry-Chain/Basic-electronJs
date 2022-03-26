d47731dada9cc044518315eeed8d09de5ef56ad8
Thierry-Chain <irambonat0@gmail.com>
2022-03-26 17:36:26 +0200

    Completed Quotes desktop app

--- main.js
+++ main.js
@@ -6,25 +6,44 @@ const path = require('path')
 
 const createWindow = () => {
   // Create the browser window.
-  const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+  let mainWindow = new BrowserWindow({
+    width: 400,
+    height: 300,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
-    }
+      preload: path.join(__dirname, 'preload.js'),
+    },
+    frame: false,
+    roundedCorners: true,
+    title: 'Inspiration quotes',
+    alwaysOnTop: true,
+    opacity: 0.95,
+    show: false,
   })
 
   // and load the index.html of the app.
   mainWindow.loadFile('index.html')
-
+  mainWindow.on('closed', () => {
+    mainWindow = null
+    // console.log('! end')
+  })
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
+  mainWindow.once('ready-to-show', () => {
+    mainWindow.show()
+  })
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.whenReady().then(() => {
+app.on('ready', createWindow)
+app.on('activate', () => {
+  // On macOS it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (BrowserWindow.getAllWindows().length === 0) createWindow()
+})
+
+/*app.whenReady().then(() => {
   createWindow()
 
   app.on('activate', () => {
@@ -32,7 +51,7 @@ app.whenReady().then(() => {
     // dock icon is clicked and there are no other windows open.
     if (BrowserWindow.getAllWindows().length === 0) createWindow()
   })
-})
+})*/
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
