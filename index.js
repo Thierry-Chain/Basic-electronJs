fa0d37c1701bd0c8b5204018c9e33eb11a92dd9d
Thierry-Chain <irambonat0@gmail.com>
2022-03-28 17:02:44 +0200

    completed basics and next is working with next/react

--- index.js
+++ index.js
@@ -1,12 +1,3 @@
-const { ipcRenderer } = require('electron')
-
-let btn = document.getElementById('test')
-
-btn.addEventListener('click', () => {
-  console.log('btn clicked')
-  // ipc.send('clicked-now')
-})
-/*
-ipc.on('handledClick', (e, arg) => {
-  console.log('over task completed', e, arg)
-})*/
+//import electron from 'electron'
+//console.log(globalThis)
+console.log('log from render process')
--- main.js
+++ main.js
@@ -1,17 +1,28 @@
-// main.js
-
-// Modules to control application life and create native browser window
-const { app, BrowserWindow, ipcMain } = require('electron')
+//const electron = require('electron')
+const {
+  app,
+  BrowserWindow,
+  ipcMain,
+  Menu,
+  MenuItem,
+  globalShortcut,
+  Tray,
+  Notification,
+  electron,
+} = require('electron')
 const path = require('path')
-
+//const { electron } = require('process')
+let mainWindow, traywin, iconPath
+iconPath = path.join(__dirname, 'capture.jpg')
 const createWindow = () => {
   // Create the browser window.
-  let mainWindow = new BrowserWindow({
-    width: 700,
-    height: 600,
+  mainWindow = new BrowserWindow({
+    width: 600,
+    height: 500,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
     },
+    alwaysOnTop: true,
   })
 
   // and load the index.html of the app.
@@ -21,13 +32,99 @@ const createWindow = () => {
     // console.log('! end')
   })
   // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
+  mainWindow.webContents.openDevTools()
   mainWindow.once('ready-to-show', () => {
     mainWindow.show()
   })
 }
 
-app.on('ready', createWindow)
+app.on('ready', () => {
+  createWindow()
+  traywin = new Tray(iconPath)
+  traywin.setToolTip('Simple app')
+  traywin.setTitle('Homemade')
+  const template = [
+    {
+      label: 'More',
+      submenu: [
+        {
+          label: 'Help',
+          click: () => {
+            electron.shell.openExternal('https://github.com/Thierry-Chain')
+          },
+        },
+        {
+          label: 'Notify',
+          click: () => {
+            const msg = new Notification({
+              title: 'Goodbye',
+              body: 'From my app',
+              urgency: 'critical',
+            })
+            msg.show()
+          },
+        },
+      ],
+    },
+    { label: 'Extract' },
+    {
+      label: 'Close',
+      submenu: [
+        {
+          label: 'Exit',
+
+          click: () => {
+            console.log('exitted')
+            app.quit()
+            app.removeAllListeners()
+            app.exit()
+          },
+        },
+        {
+          role: 'selectall',
+        },
+        {
+          role: 'reload',
+        },
+        {
+          role: 'copy',
+        },
+      ],
+    },
+  ]
+  const temp1 = [
+    {
+      label: 'Exit',
+      click: () => {
+        console.log('exitted')
+        app.quit()
+        app.removeAllListeners()
+        app.exit()
+      },
+    },
+  ]
+  const menu = Menu.buildFromTemplate(template)
+  const menu1 = Menu.buildFromTemplate(temp1)
+  traywin.setContextMenu(menu1)
+  Menu.setApplicationMenu(menu)
+  const ctxMenu = new Menu()
+  ctxMenu.append(
+    new MenuItem({
+      label: 'Inspect',
+      click: () => {
+        console.log('click from context menu')
+      },
+      accelerator: 'Alt + q',
+    }),
+  )
+  ctxMenu.append(new MenuItem({ role: 'selectAll' }))
+  mainWindow.webContents.on('context-menu', (e, params) => {
+    ctxMenu.popup(mainWindow, params.x, params.y)
+  })
+  globalShortcut.register('Alt+0', () => {
+    mainWindow.show()
+  })
+})
 app.on('activate', () => {
   if (BrowserWindow.getAllWindows().length === 0) createWindow()
 })
